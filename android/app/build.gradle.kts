a2a12cc8cb4752ca736cb5f63946cb62cb8a9c16
vishal <beradvishal2005@gmail.com>
2026-07-16 10:05:52 +0530

    project run

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.axolotls.grofery_rider"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
